5364be83871450af4cf9470277ce60c534f95cf3
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-12 22:18:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@
 Import os
 from pyrogram import Client, filters 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN") 
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH") 
     CAPTION = os.environ.get("CAPTION")
