2119dc9c918104a063c96b4f1d81610fddeb1a2a
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-12 22:19:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@
 Import os
 from pyrogram import Client, filters 
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN") 
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH") 
-    CAPTION = os.environ.get("CAPTION")
+BOT_TOKEN = os.environ.get("BOT_TOKEN") 
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH") 
+CAPTION = os.environ.get("CAPTION")
 
 #experiment #fail
 async def get_size(size):
